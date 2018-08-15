47d1357664d9521c4710fe37bc63b55fbd64e2ac
parth <parthsethi16.ps@gmail.com>
2018-08-14 22:12:59 -0400

    Express REST API with MongoDB and Mongoose Part 1

--- app.js
+++ app.js
@@ -8,9 +8,18 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const dishRouter = require('./routes/dishRouter');
 const leaderRouter = require('./routes/leaderRouter');
-
 const promoRouter = require('./routes/promoRouter');
 
+const mongoose = require('mongoose');
+
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+    console.log("Connected correctly to server");
+}, (err) => { console.log(err); });
 
 var app = express();
 
